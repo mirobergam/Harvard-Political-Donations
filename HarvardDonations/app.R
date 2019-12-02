5a722ade9793d728b3b72150ced1790c2b03947f
Miroslav Bergam <mlbergam@gmail.com>
2019-12-02 18:34:17 -0500

    Added general findings/summary page.

--- HarvardDonations/app.R
+++ HarvardDonations/app.R
@@ -148,11 +148,11 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                 
                 tabPanel("Summary",
                          h3("General Findings"),
-                         p("This project looks to analyze how individuals employed by Harvard University donate their political dollars. I visualize in multiple different ways the size of the donations, the support different candidates recieve, and how political ideology impacts donations. I also look at how these donations differ within the graduate schools and how all of the donations compare to the general population's donations."),
-                         p("I accessed data on the political donations of individuals employed by Harvard University on the website of the Federal Election Committee, which releases information on every donation made to a presidential, senate, or house political campaign, including the name of the donor, their occupation, their employer, the donated amount, their state, and the recipient campaign."),
-                         p("Although this data was easily accessible by downloading a csv file, there was much data cleaning to be done to ensure that my analyses are accurate. First, I only observed donations made to 2020 presidential campaigns, excluding senate and house donations. Second, I filtered for donations made after January 1st, 2017, as there was no presidential donations to 2020 campaigns in 2016 or earlier. Third, I had to parse through the contributor employer data and filter out donations whose donors were not actually employed by Harvard University. For example, the donations from the employees of Harvard-Westlake School were present in the data, as they contain the word “Harvard.” Donations like this were removed."),
-                         p("I also used the Federal Election Committee website to find data on the campaign donations of the general population, the total campaign sizes, and the political party affiliations of the different committees."),
-                         p("Additionally, I sourced data from a DataForProgress.org article, which offered ideology scores for 2020 candidates who served in Congress. The specific data was not publicly availible, and I had to reach out to the article author to access it.")
+                         p("Elizabeth Warren, by most measures, has the most support from Harvard-employed donors. Candidates who attended Harvard have disproportionate support within the Harvard community."),
+                         p("HBS employees donate the most money to political campaigns, but HKS employees the largest amounts on average."),
+                         p("Educators employed by Harvard donate higher amounts than non-educators employed by Harvard. More conservative candidates generally recieve larger donations."),
+                         p("Less popular candidates recieve larger donations from Harvard-employed donors, while more popular candidates have more individual donors.")
+                         
                 ))
 )
 
@@ -497,7 +497,10 @@ server <- function(input, output) {
       # Creating a coefficient plot displaying the coefficients of ideological
       # estimate and educatorship
       
-      m1 <- lm(data = data, contribution_receipt_amount ~ professor + estimate)
+      data <- data %>%
+        mutate(ideology = estimate)
+      
+      m1 <- lm(data = data, contribution_receipt_amount ~ professor + ideology)
       
       #Visualize the coefficients using coefplot
       
